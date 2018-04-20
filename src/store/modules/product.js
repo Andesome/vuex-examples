ce73ae1698906ee355b47dda4263ce6706223246
lyin <andesome95@gmail.com>
2018-04-20 18:19:26 +0800

    add cart,product vuex modules

---
+++ src/store/modules/product.js
@@ -0,0 +1,58 @@
+
+const state = {
+  list: [{
+    id:1,
+    title:'小米5',
+    price:2999,
+    inventory:9
+  },{
+    id:2,
+    title:'iphone 8 plus',
+    price:5999,
+    inventory:5
+  },{
+    id:3,
+    title:'锤子手机T1',
+    price:1999,
+    inventory:3
+  }]
+}
+
+// 这里是实时计算后的state
+const getters = {
+  list: state => state.list
+}
+
+
+// 这里要被触发的动作
+const actions = {
+  increment: ({ commit }) => commit('increment'),  //计数器+1
+  decrement: ({ commit }) => commit('decrement'),   //计数器-1
+  incrementAsync: ({ commit }) => {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        commit('increment');
+        resolve();
+      },1000)
+    })
+  }
+}
+
+// 这里是等待被action触发的方法
+const mutations = {
+  increment(state) {
+    state.count++;
+  },
+  decrement(state) {
+    state.count--
+  }
+}
+
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
+
